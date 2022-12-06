43ede94e7d65682b2eefaf644f16752c4508a016
deybyr647 <deybyr647@gmail.com>
2022-12-06 16:37:41 -0500

    Initial Commit

---
+++ Day1.swift
@@ -0,0 +1,40 @@
+/*  
+  iOS Club 2023 - Day 1 Sample Code
+  Day1.swift  
+  Created by Deyby Rodriguez on 12/6/22.
+*/
+
+// Creating & Updating Variables
+var greeting = "Hello Engineer!"
+greeting = "Thank you for coding in Swift"
+print(greeting)
+
+// Constants
+let pi = 3.14159
+
+// Uncommenting the following line would cause an error to be thrown
+// pi = 3
+
+// String Creation & Type Annotations
+var curriculumCreator: String = "Coach Jeremy"
+
+var g: Float = 9.81
+
+// Uncommenting the following line would cause an error, since g is a Float, not a String
+// g = "9"
+
+
+// Fun with Numbers
+var siblingCount: Int = 3
+
+// Underscores function as commas here. This is a valid way to express a number
+var bankAccountBalance: Int = 1_000_000
+
+// Constants are useful for storing important floats, like e and pi
+let e: Float = 2.71828
+let newYorkStateSalesTax: Float = 0.08875
+
+// Boolean values, for expressing things that are either true or false
+let isTired: Bool = true
+var isHungry: Bool = true
+var wearsGlasses: Bool = false
